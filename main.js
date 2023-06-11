8f436724d995e7c6a730b9429bc0285f785c7637
Soulaimane <82826040+soulaimane0@users.noreply.github.com>
2023-06-11 16:23:17 +0100

    update changes

--- main.js
+++ main.js
@@ -3,50 +3,95 @@ const player1 = document.querySelector('#p1');
 const player2 = document.querySelector('#p2');
 const player1Score = document.querySelector('#p1_score');
 const player2Score = document.querySelector('#p2_score');
+const player2Title = document.querySelector('#p2_title');
+const moodElem = document.querySelector('#playMood');
+const onePlayerMood = document.querySelector('#oneP');
+const twoPlayersMood = document.querySelector('#twoP');
 
 let playerTurn = 'X';
 let p1_score = 0;
 let p2_score = 0;
+let isOnePlayerMood = false;
 
+// Game Sounds
 const note_high = document.querySelector('#note_high');
 const note_low = document.querySelector('#note_low');
-const gale_over = document.querySelector('#game_over');
+const game_over = document.querySelector('#game_over');
+const game_over_tie = document.querySelector('#game_over_tie');
 
-const toggleActiveClass = (elem, action) => {
-  if (action === 'add') elem.classList.add('active');
-  else if (action === 'remove') elem.classList.remove('active');
+const toggleClassName = (elem, action, className) => {
+  if (action === 'add') elem.classList.add(className);
+  else if (action === 'remove') elem.classList.remove(className);
 };
+
+toggleClassName(onePlayerMood, 'add', 'hidden');
+
+moodElem.addEventListener('click', () => {
+  isOnePlayerMood = !isOnePlayerMood;
+  if (isOnePlayerMood === true) {
+    toggleClassName(onePlayerMood, 'remove', 'hidden');
+    toggleClassName(twoPlayersMood, 'add', 'hidden');
+    player2Title.textContent = 'Computer (O)';
+  } else {
+    toggleClassName(twoPlayersMood, 'remove', 'hidden');
+    toggleClassName(onePlayerMood, 'add', 'hidden');
+    player2Title.textContent = 'Player 2 (O)';
+  }
+  p1_score = 0;
+  p2_score = 0;
+  console.log('is One player mood : ', isOnePlayerMood);
+});
 
 player1Score.textContent = p1_score;
 player2Score.textContent = p2_score;
 
-toggleActiveClass(player1, 'add');
+toggleClassName(player1, 'add', 'active');
 const switchPlayer = () => {
   if (playerTurn === 'X' && !gameWinner(board)) {
     note_high.play();
     playerTurn = 'O';
-    toggleActiveClass(player1, 'remove');
-    toggleActiveClass(player2, 'add');
+    toggleClassName(player1, 'remove', 'active');
+    toggleClassName(player2, 'add', 'active');
   } else if (playerTurn === 'O' && !gameWinner(board)) {
     note_low.play();
     playerTurn = 'X';
-    toggleActiveClass(player2, 'remove');
-    toggleActiveClass(player1, 'add');
+    toggleClassName(player2, 'remove', 'active');
+    toggleClassName(player1, 'add', 'active');
   }
 };
 
-const squareClick = (index) => {
+const makeMove = (index, square) => {
+  board[index] = playerTurn;
+  const spanElm = document.createElement('span');
+  spanElm.textContent = playerTurn;
+  square.appendChild(spanElm);
+
+  switchPlayer();
+};
+
+function squareClick(index) {
   const square = document.getElementById(`square_${index}`);
 
   if (square.innerHTML == '' && !gameWinner(board)) {
-    board[index] = playerTurn;
-    const spanElm = document.createElement('span');
-    spanElm.textContent = playerTurn;
-    square.appendChild(spanElm);
+    makeMove(index, square);
 
-    switchPlayer();
+    if (isOnePlayerMood) {
+      let computerIndex;
+
+      do {
+        computerIndex = Math.floor(Math.random() * board.length);
+      } while (board[computerIndex] !== null && !gameWinner(board));
+      if (!gameWinner(board)) {
+        const computerSquare = document.getElementById(
+          `square_${computerIndex}`
+        );
+        setTimeout(() => {
+          makeMove(computerIndex, computerSquare);
+        }, 300);
+      }
+    }
   }
-};
+}
 
 const createBoardSquares = () => {
   const gameBoard = document.querySelector('#board');
@@ -102,14 +147,16 @@ const gameWinner = (board) => {
       player2Score.textContent = p2_score;
 
       restartGame(board);
-      gale_over.play();
+      game_over.play();
       return board[a];
     } else if (
       !board.includes(null) &&
       board[a] !== board[b] &&
       board[a] !== board[c]
     ) {
+      game_over_tie.play();
       restartGame(board);
+      return board[a];
     }
   }
   return null;
